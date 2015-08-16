5ab06df18f557beadcd0735649c0e5662e062f7f
Milos Colakovic <gmefox@gmail.com>
2015-08-17 00:09:46 +0200

    Add UID transformer

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -3,7 +3,17 @@
 namespace Undine\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use Undine\Model\User;
 
 class UserRepository extends EntityRepository
 {
+    /**
+     * @param string $uid
+     *
+     * @return User|null
+     */
+    public function findByUid($uid)
+    {
+        return $this->find(User::getIdFromUid($uid));
+    }
 }
