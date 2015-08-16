0aa38d224ad8dc465953bb63494ffd6295513f63
Milos Colakovic <gmefox@gmail.com>
2015-08-16 22:05:15 +0200

    Expand app controller

--- src/AppBundle/Controller/AppController.php
+++ src/AppBundle/Controller/AppController.php
@@ -2,11 +2,33 @@
 
 namespace Undine\AppBundle\Controller;
 
+use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Undine\Model\User;
+use Undine\Repository\StaffRepository;
+use Undine\Repository\UserRepository;
 
-class AppController extends Controller
+/**
+ * @property EntityManager   $em
+ * @property UserRepository  $userRepository
+ * @property StaffRepository $staffRepository
+ */
+abstract class AppController extends Controller
 {
+    function __get($name)
+    {
+        switch ($name) {
+            case 'em':
+                return $this->container->get('doctrine.orm.default_entity_manager');
+            case 'userRepository':
+                return $this->container->get('doctrine.repository.user');
+            case 'staffRepository':
+                return $this->container->get('doctrine.repository.staff');
+            default:
+                throw new \BadMethodCallException();
+        }
+    }
+
     /**
      * @return User
      */
