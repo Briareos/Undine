76ff7d1aac7ddb71a662ef1cc059ecfad5bf544c
Nemanja Miljkovic <proof1337@gmail.com>
2015-09-06 22:56:26 +0200

    Create a state change listener to redirect to add website if no websites are present

--- frontend/dashboard/app/run.ts
+++ frontend/dashboard/app/run.ts
@@ -1,4 +1,25 @@
 angular.module('undine.dashboard')
     .run(function ($rootScope, $window) {
         $rootScope.currentUser = $window.appData.currentUser;
+    })
+    .run(function (Dashboard:DashboardInterface, $state:ng.ui.IStateService, $rootScope:ng.IRootScopeService) {
+        // Listen for state change events.
+        // If the state depends on site picker being visible and there are no sites added, redirect to add website state.
+        $rootScope.$on('$stateChangeStart', function ($event:ng.IAngularEvent, toState:ng.ui.IState) {
+            // temporarily disabled
+            return;
+
+            if (Dashboard.sites.length || toState.name === 'add-website') {
+                return;
+            }
+
+            if (_.isUndefined(toState.data) ||
+                _.isUndefined(toState.data.sitePicker) ||
+                _.isUndefined(toState.data.sitePicker.visible) || !toState.data.sitePicker.visible) {
+                return;
+            }
+
+            $event.preventDefault();
+            $state.go('add-website');
+        });
     });
