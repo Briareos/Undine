42f1713926ff4fdd3d86a3a5c1b9bca583d25c5b
Milos Colakovic <gmefox@gmail.com>
2016-01-24 15:05:13 +0100

    Add basic auth to dev endpoint

--- web/app_dev.php
+++ web/app_dev.php
@@ -24,9 +24,9 @@ $httpAuth = function () {
     exit;
 };
 
-//if (!in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1', '10.0.2.2'])) {
+if (!in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1', '10.0.2.2'])) {
     $httpAuth();
-//}
+}
 unset($httpAuth);
 
 /**
